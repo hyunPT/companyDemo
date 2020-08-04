c86ffa6175ba4ef6052021d26ab7353f1cf6c9c3
hyunPT <51047350+hyunPT@users.noreply.github.com>
2020-08-05 08:46:21 +0900

    Update DBConnection.java

--- companyDemo/src/mvc/database/DBConnection.java
+++ companyDemo/src/mvc/database/DBConnection.java
@@ -8,9 +8,9 @@ public class DBConnection {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException{
 		Connection conn= null;
 		
-		String url = "jdbc:mysql://34.64.223.143:3306/companyDemo";
-		String user = "remote";
-		String password = "1pq02ow9";
+		String url = "jdbc:mysql://ip:port/database";
+		String user = "id";
+		String password = "pw";
 		
 		Class.forName("com.mysql.jdbc.Driver");
 		conn = DriverManager.getConnection(url,user,password);
